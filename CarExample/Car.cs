dd362879d794393b7bf0b957b18547f4cabed8bb
zano <adrian@xenotype.com>
2019-03-28 17:01:18 +0100

    Init

---
+++ CarExample/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CarExample
+{
+    class Car
+    {
+        public string RegNo { get; set; }
+
+        private readonly double fuelConsumtion = 0.1;
+
+        private double fuelLevel;
+        
+        public Car(string regNo)
+        {
+            RegNo = regNo;
+        }
+
+        public double FuelLevel
+        {
+            get { return fuelLevel; }
+            set {
+                double newLevel = Math.Max(0, value);
+                fuelLevel = Math.Min(newLevel, FuelCapacity);
+            }
+        }
+
+        public double FuelCapacity {
+            get;
+            private set;
+        } = 105;
+
+        public double Mileage { get; set; }
+        public static double GlobalMileage { get; private set; }
+
+
+        public void Drive(double distanceKm)
+        {
+            Console.WriteLine(RegNo + " vill köra " + distanceKm + " km." );
+            if (distanceKm < 0)
+            {
+                distanceKm = 0;
+                Console.WriteLine("Negativt avstånd tolkas som noll");
+            }
+
+            double maxDistance = fuelLevel / fuelConsumtion;
+            if (distanceKm > maxDistance)
+            {
+                distanceKm = maxDistance;
+                Console.WriteLine("Bränslet räcker inte så långt.");
+            }
+
+            fuelLevel -= distanceKm * fuelConsumtion;
+            Mileage += distanceKm;
+            GlobalMileage += distanceKm;
+
+            Console.WriteLine(RegNo + " körde " + distanceKm + " km.");
+            Console.WriteLine();            
+        }
+
+        public void StatusReport() {            
+            Console.WriteLine("RegNo:   " + RegNo);
+            Console.WriteLine("Fuel:    " + FuelLevel + "/" + FuelCapacity);
+            Console.WriteLine("Mileage: " + Mileage);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CarExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CarExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car(regNo: "ABC127") {
+                FuelLevel = -10,
+                Mileage = 100
+            };
+
+            Car car2 = new Car("XYZ987");
+
+            car1.StatusReport();
+            car2.StatusReport();
+
+            Console.WriteLine("Odometer: " + Car.GlobalMileage);
+            Console.WriteLine();
+           
+            // tanka             
+            car1.FuelLevel += 100;
+            car2.FuelLevel += 50;
+
+            // kör en massa
+            car1.Drive(600);
+            car1.Drive(600);
+            car2.Drive(100000);
+
+            car1.StatusReport();
+            car2.StatusReport();
+            System.Console.WriteLine("Odometer: " + Car.GlobalMileage);
+
+
+        }
+    }
+}
